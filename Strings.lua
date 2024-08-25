5eeb4a12180788d0f009e049c5649a761ca461e8
Ebola Man <130239273+EzotaCreator@users.noreply.github.com>
2024-08-25 20:59:21 +0200

    Update Strings.lua

--- Strings.lua
+++ Strings.lua
@@ -1,6 +1,6 @@
 -- Collection of strings later formatted by the decompiler for output
 local Strings = {
-	SUCCESS = "-- Decompiled with the Synapse X Luau decompiler.\n--Full credits for the disassembler/decompiler go to w-a-e found here: %s",
+	SUCCESS = "-- Decompiled with the Synapse X Luau decompiler.\n--Full credits for the disassembler/decompiler go to w-a-e found here: https://github.com/w-a-e/Advanced-Decompiler-V3\n%s",
 	TIMEOUT = "-- DECOMPILER TIMEOUT",
 	COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
 	UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
